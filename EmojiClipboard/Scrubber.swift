0f49cc8df64dc670c3aca175c941202d7b54e17f
Ilia Lesik <ilialesik@gmail.com>
2019-03-24 19:45:38 +0300

    Implement emoji groups
    Install Smile.

--- EmojiClipboard/Scrubber.swift
+++ EmojiClipboard/Scrubber.swift
@@ -6,6 +6,7 @@
 //
 
 import Cocoa
+import Smile
 
 @available(OSX 10.12.2, *)
 class Scrubber: NSCustomTouchBarItem, NSScrubberDelegate, NSScrubberDataSource, NSScrubberFlowLayoutDelegate {
@@ -38,20 +39,20 @@ class Scrubber: NSCustomTouchBarItem, NSScrubberDelegate, NSScrubberDataSource, 
     }
     
     func scrubber(_ scrubber: NSScrubber, viewForItemAt index: Int) -> NSScrubberItemView {
-        
-        // if the array is empty, get frequently used emojis:
-        if(Emojis.arrayEmojis.count == 0) {
-            Emojis.arrayEmojis = Emojis.getAllEmojis()
-        }
-        
         let itemView = scrubber.makeItem(withIdentifier: self.itemViewIdentifier,
                                          owner: nil) as! NSScrubberTextItemView
         
+        let emojiToFiew = Emojis.arrayEmojis[index]
+        
         // (scrubber iterates depending on value returned by numberOfItems)
-        itemView.textField.stringValue = Emojis.arrayEmojis[index]
+        itemView.textField.stringValue = emojiToFiew.value
         
         // change emoji font size:
-        itemView.textField.font = NSFont.systemFont(ofSize: 28)
+        if (emojiToFiew.type == "item") {
+            itemView.textField.font = NSFont.systemFont(ofSize: 28)
+        } else {
+            itemView.textField.font = NSFont.systemFont(ofSize: 14)
+        }
         
         return itemView
     }
@@ -59,19 +60,27 @@ class Scrubber: NSCustomTouchBarItem, NSScrubberDelegate, NSScrubberDataSource, 
     func scrubber(_ scrubber: NSScrubber, layout: NSScrubberFlowLayout, sizeForItemAt itemIndex: Int) -> NSSize {
         
         // set the distance between emojis (the height Touch Bar is 30 points):
-        return NSSize(width: scrubberItemWidth, height: 30)
+        let emojiToType = Emojis.arrayEmojis[itemIndex]
+        if emojiToType.type == "item" {
+            return NSSize(width: scrubberItemWidth, height: 30)
+        } else {
+           return NSSize(width: emojiToType.value.count * 10, height: 30)
+        }
     }
 
     func scrubber(_ scrubber: NSScrubber, didSelectItemAt index: Int) {
-        // check permissions:
-        AXIsProcessTrustedWithOptions([kAXTrustedCheckOptionPrompt.takeRetainedValue() as NSString: true] as NSDictionary)
-        
         // get the emoji that has been selected:
         let emojiToType = Emojis.arrayEmojis[index]
+        if Smile.isEmoji(character: emojiToType.value) {
+            // check permissions:
+            AXIsProcessTrustedWithOptions([kAXTrustedCheckOptionPrompt.takeRetainedValue() as NSString: true] as NSDictionary)
+            
+            KeyEvent.typeEmoji(emoji: emojiToType.value)
+            
+            // deselect the selected emoji:
+            scrubber.selectedIndex = -1
+        }
         
-        KeyEvent.typeEmoji(emoji: emojiToType)
         
-        // deselect the selected emoji:
-        scrubber.selectedIndex = -1
     }
 }
